4de4f049a0595248ce3bbb1b7480aa7a72314804
Patrice Ferlet <metal3d@gmail.com>
2021-03-05 13:10:59 +0100

    Use Queue to enhance performance on tests

--- tests/test_feed.py
+++ tests/test_feed.py
@@ -1,6 +1,7 @@
 """Test feed generator."""
 
 import time
+from queue import Queue
 from threading import Thread
 from typing import Optional
 from unittest.case import TestCase
@@ -39,29 +40,43 @@ class FeedTest(TestCase):
         first_call_name = "first"
         second_call_name = "second"
 
+        queue = Queue()
+
         def wait_for_changes():
             feed = FeededUser.changes()
+            queue.put(True)
             old, new = next(feed)
             self.assertIsNone(old)
             self.assertEqual(new.name, first_call_name)
+            queue.put(True)
             old, new = next(feed)
             self.assertEqual(old.name, first_call_name)
             self.assertEqual(new.name, second_call_name)
 
         thread = Thread(target=wait_for_changes)
         thread.start()
-        time.sleep(3)  # we need to wait a bit...
+        # wait for thread to be ready
+        queue.get()
+        time.sleep(1)
+        # firs save
         user = FeededUser(name=first_call_name).save()
+
+        # change name
+        queue.get()
         time.sleep(1)
         user.name = second_call_name
         user.save()
+
+        # stop
         thread.join()
 
     def test_with_filter(self):
         """Test to get a feed with a filter."""
+        queue = Queue()
 
         def filter_user():
             feed = FeededUser.changes(select=lambda res: res["name"].eq("foo"))
+            queue.put(True)
             # feed = FeededUser.changes()
             old, new = next(feed)
             self.assertIsNone(old)
@@ -72,14 +87,13 @@ class FeedTest(TestCase):
 
         thread = Thread(target=filter_user)
         thread.start()
-        time.sleep(3)
-        FeededUser(name="bar1").save()  # should fail
-        time.sleep(1)
+        queue.get(True)
+        FeededUser(name="bar1").save()  # fail if it's captured
         FeededUser(name="foo").save()  # should be ok
         thread.join()
 
 
-class FeetOnDeletedTest(TestCase):
+class FeedOnDeletedTest(TestCase):
     """Make test on deleted objects."""
 
     def setUp(self) -> None:
@@ -89,9 +103,11 @@ class FeetOnDeletedTest(TestCase):
 
     def test_on_solft_deleted(self):
         """Make test on a deleted feeduser."""
+        queue = Queue()
 
         def filter_user():
             feed = FeededUser.changes(select=lambda res: res["name"].eq("foo"))
+            queue.put(True)
             old, new = next(feed)
             if "foo" not in new.name and new.pointer is not None:
                 self.fail(
@@ -116,13 +132,17 @@ class FeetOnDeletedTest(TestCase):
 
         thread = Thread(target=filter_user)
         thread.start()
-        time.sleep(3)
+        # wait for thread to be ready
+        queue.get()
+        time.sleep(1)
+
         # this will fail if the object is catched
         rdb.table(FeededUser.tablename).get(kept).update(
             {"name": "foo", "pointer": "changed"}
         ).run(conn)
 
         # to close the thread
+        time.sleep(1)
         FeededUser(name="foo").save()
         thread.join()
 
